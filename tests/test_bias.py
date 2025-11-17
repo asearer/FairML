2c678f1da3322c584f8df5eecacfaf44fdbc88f9
asearer <amsearer84@gmail.com>
2025-11-17 17:03:04 -0500

    CI updates

--- tests/test_bias.py
+++ tests/test_bias.py
@@ -5,16 +5,21 @@ import sys
 import pandas as pd
 
 # Ensure src is on the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")),
+)
 
 from fairness_tests.bias import introduce_bias
 
 
 def test_introduce_bias():
-    df = pd.DataFrame({
-        "feature": [0, 1, 0, 1],
-        "target": [0, 1, 0, 1],
-    })
+    df = pd.DataFrame(
+        {
+            "feature": [0, 1, 0, 1],
+            "target": [0, 1, 0, 1],
+        }
+    )
 
     biased = introduce_bias(df, "feature", bias_amount=0.2)
 
